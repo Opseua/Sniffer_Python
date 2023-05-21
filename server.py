0ea1408123cd0504579ce30c4a8e524dd4a620da
Orlando <opseua@gmail.com>
2023-05-21 01:20:27 -0300

    Correção do headers

--- server.py
+++ server.py
@@ -1,44 +1,74 @@
 import subprocess
 from mitmproxy import http, ctx
 import argparse
+import fnmatch
+import json
+
+# Definir os URLs que devem ser interceptados
+urls_interceptadas = [
+    "https://ntfy.sh/",
+    "https://rating.ewoq.google.com/u/0/rpc/rating/AssignmentAcquisitionService/GetNewTasks",
+    "*.alguma.com/*"
+]
 
 def escreverEmArquivo(texto, caminho_arquivo, modo):
-    with open(caminho_arquivo, modo) as arquivo:
-        arquivo.write(texto + "\n")
+    with open(caminho_arquivo, modo, encoding='utf-8') as arquivo:
+        arquivo.write(texto)
 
 def request(flow: http.HTTPFlow) -> None:
-    if flow.request.url == "ntfy.sh" or flow.request.url.startswith("https://rating.ewoq.google.com"):
-        # Exibir a URL no console 1
-        texto = " ".join(["URL:", flow.request.url])
-        escreverEmArquivo(texto, "LOG.txt", "a")
+    # Verificar se a URL do request corresponde a algum dos URLs especificados
+    for url in urls_interceptadas:
+        if fnmatch.fnmatch(flow.request.url, url):
+            # Converter os headers para o formato JSON
+            headers_json = {name: value for name, value in flow.request.headers.items()}
+            headers_str = json.dumps(headers_json)
 
-        texto = " ".join(["METODO:", flow.request.method])
-        escreverEmArquivo(texto, "LOG.txt", "a")
+            # Substituir "CASA" por "AAAAAA" no corpo do request
+            if flow.request.method != "GET" and flow.request.content:
+                # Decodificar o corpo da requisição
+                body = flow.request.get_text()
 
-        texto = " ".join(["HEADERS:", str(flow.request.headers)])
-        escreverEmArquivo(texto, "LOG.txt", "a")
+                # Substituir "CASA" por "AAAAAA"
+                modified_body = body.replace("CASA", "AAAAAA")
 
-        escreverEmArquivo(flow.request.get_text(), "LOG.txt", "a")        
+                # Atualizar o corpo da requisição
+                flow.request.set_text(modified_body)
 
-    # Verificar se a requisição possui um corpo
-    if flow.request.method != "GET" and flow.request.content:
-        # Decodificar o corpo da requisição
-        body = flow.request.get_text()
-        
-        # Verificar se a palavra "CASA" está presente no corpo
-        if "CASA" in body:
-            # Substituir "CASA" por "AAAAAA"
-            modified_body = body.replace("CASA", "AAAAAA")
-            
-            # Atualizar o corpo da requisição
-            flow.request.set_text(modified_body)
-            
-            # Registrar a substituição
-            ctx.log.info("Palavra 'CASA' substituída por 'AAAAAA' na requisição.")
+                # Registrar a substituição
+                ctx.log.info("Palavra 'CASA' substituída por 'AAAAAA' na requisição.")
+
+            # LOG REQUEST
+            texto = "############ REQUEST ############\n"
+            texto += "URL: " + flow.request.url + "\n"
+            texto += "METODO: " + flow.request.method + "\n"
+            texto += "HEADERS: " + headers_str + "\n"
+            texto += "BODY: " + flow.request.get_text() + "\n\n"
+            escreverEmArquivo(texto, "LOG.txt", "a")
+            break
 
 def response(flow: http.HTTPFlow) -> None:
-    # Log da resposta recebida do servidor
-    ctx.log.info("R")
+    # Verificar se a URL do response corresponde a algum dos URLs especificados
+    for url in urls_interceptadas:
+        if fnmatch.fnmatch(flow.request.url, url):
+            # Converter os headers para o formato JSON
+            headers_json = {name: value for name, value in flow.response.headers.items()}
+            headers_str = json.dumps(headers_json)
+
+            # Verificar se a resposta possui um corpo
+            if flow.response.content:
+                # Decodificar o corpo da resposta
+                body = flow.response.get_text()
+
+                # Verificar se o corpo tem mais de um caractere
+                if len(body) > 1:
+                    # LOG RESPONSE
+                    texto = "############ RESPONSE ############\n"
+                    texto += "URL: " + flow.request.url + "\n"
+                    texto += "METODO: " + flow.request.method + "\n"
+                    texto += "HEADERS: " + headers_str + "\n"
+                    texto += "BODY: " + body + "\n\n"
+                    escreverEmArquivo(texto, "LOG.txt", "a")
+            break
 
 def main(port):
     # Chamar o script enableProxy.py passando o número da porta como argumento

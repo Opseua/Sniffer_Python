75befbc9b1f508fb787222011e3b961ff0732daa
Orlando <opseua@gmail.com>
2023-09-12 04:11:33 -0300

    Corrigido spam

--- src/sniffer.js
+++ src/sniffer.js
@@ -3,16 +3,24 @@ await import('../../Chrome_Extension/src/resources/@functions.js'); import net f
 try {
     async function run() {
         let infConfigStorage, retConfigStorage
+
         infConfigStorage = { 'action': 'get', 'key': 'sniffer' }; retConfigStorage = await configStorage(infConfigStorage)
-        if (!retConfigStorage.ret) { return ret } else { retConfigStorage = retConfigStorage.res }
-        const portSocket = retConfigStorage.portSocket; const bufferSocket = retConfigStorage.bufferSocket; const arrUrl = retConfigStorage.arrUrl
+        if (!retConfigStorage.ret) { return } else { retConfigStorage = retConfigStorage.res }
+        const portSocket = retConfigStorage.portSocket; const bufferSocket = retConfigStorage.bufferSocket;
+        const arrUrl = retConfigStorage.arrUrl
+
         infConfigStorage = { 'action': 'get', 'key': 'webSocket' }; retConfigStorage = await configStorage(infConfigStorage)
-        if (!retConfigStorage.ret) { return ret } else { retConfigStorage = retConfigStorage.res }
+        if (!retConfigStorage.ret) { return } else { retConfigStorage = retConfigStorage.res }
         const wsHost = retConfigStorage.ws1; const portWebSocket = retConfigStorage.portWebSocket;
         const securityPass = retConfigStorage.securityPass; const device1 = retConfigStorage.device1.name;
         const device1Ret = retConfigStorage.device1.ret; const device2 = retConfigStorage.device2.name;
         const device2Ret = retConfigStorage.device2.ret; let infFile, retFile;
-        infFile = { 'action': 'inf' }; retFile = await file(infFile); if (!retFile.ret) { return ret } else { retFile = retFile.res }
+
+        infConfigStorage = { 'action': 'get', 'key': 'platforms' }; retConfigStorage = await configStorage(infConfigStorage)
+        if (!retConfigStorage.ret) { return } else { retConfigStorage = retConfigStorage.res }
+        const tryRating = retConfigStorage.tryRating;
+
+        infFile = { 'action': 'inf' }; retFile = await file(infFile); if (!retFile.ret) { return } else { retFile = retFile.res }
         let command = `"${conf[1]}:\\ARQUIVOS\\WINDOWS\\BAT\\RUN_PORTABLE\\1_BACKGROUND.exe"`
         command = `${command} "${conf[1]}:\\ARQUIVOS\\WINDOWS\\PORTABLE_Python\\python-3.11.1.amd64\\python.exe"`
         command = `${command} "${conf[1]}:\\ARQUIVOS\\PROJETOS\\Sniffer_Python\\src\\resources\\start.py"`
@@ -111,10 +119,9 @@ try {
                                     "funRun": {
                                         "name": "notification",
                                         "par": {
-                                            "duration": 5,
+                                            "duration": 5, "iconUrl": "./src/media/notification_3.png",
                                             "title": `ALERTA`,
-                                            "message": "Outro tipo de tarefa!",
-                                            "iconUrl": "./src/media/notification_3.png",
+                                            "message": "Outro tipo de tarefa!"
                                         }
                                     }
                                 }
@@ -139,18 +146,38 @@ try {
                         const dif = Number(time.tim) - Number(retFile.res)
 
 
-                        const nameTask = `${JSON.parse(jsonSend).data.templateTaskType.replace(/[^a-zA-Z0-9]/g, '')}`;
-                        let infConfigStorage, retConfigStorage; let json
+                        const hitApp = `${JSON.parse(jsonSend).data.templateTaskType.replace(/[^a-zA-Z0-9]/g, '')}`;
+                        let infConfigStorage, retConfigStorage, json
                         infConfigStorage = { 'path': `./log/TryRating/${time1}/###_JSON_###.json`, 'functionLocal': false, 'action': 'get', 'key': 'tryRating' }
                         retConfigStorage = await configStorage(infConfigStorage);
-                        if (!retConfigStorage.ret) { json = {}; json['tryRating'] = {}; json = json['tryRating'] } else { json = retConfigStorage.res }
-                        if (!json[nameTask]) { json[nameTask] = { 'inf': {}, 'tasks': [] } };
+                        if (!retConfigStorage.ret) { json = { 'inf': { 'reg': { 'tasksQtd': 0, 'tasksSec': 0, }, 'taskName': {} }, 'tasks': [] } }
+                        else { json = retConfigStorage.res };
                         const jsonInf1 = new Date(Number(`${retFile.res}000`)).toLocaleTimeString(undefined, { hour12: false });
-                        const jsonInf2 = new Date(Number(time.timMil)).toLocaleTimeString(undefined, { hour12: false });
-                        json[nameTask].tasks.push({ 'start': jsonInf1, 'end': jsonInf2, 'sec': dif });
+                        const jsonInf2 = new Date(Number(`${time.tim}000`)).toLocaleTimeString(undefined, { hour12: false });
+                        json.tasks.push({ 'taskName': hitApp, 'start': jsonInf1, 'end': jsonInf2, 'sec': dif });
+                        let tasksQtd = 0, tasksSec = 0, tasksQtdHitApp = 0, tasksSecHitApp = 0, tasksQtdHitAppLast = 0, tasksSecHitAppLast = 0, lastHour, tasksQtdMon = 0, tasksSecMon = 0
+                        if (!tryRating[hitApp]) { lastHour = tryRating.default.lastHour } else { lastHour = tryRating[hitApp].lastHour }
+                        json.tasks.map(async (value, index) => {
+                            tasksQtd += 1; tasksSec += value.sec
+                            if (value.taskName == hitApp) {
+                                tasksQtdHitApp += 1; tasksSecHitApp += value.sec
+                                const timestamp = new Date(`2023-${time.mon}-${time.day}T${value.start}`).getTime();
+                                if (timestamp + lastHour * 1000 > Number(time.timMil)) { tasksQtdHitAppLast += 1; tasksSecHitAppLast += value.sec }
+                            }
+                        })
+                        json.inf.reg = { 'tasksQtd': tasksQtd, 'tasksSec': tasksSec }
+                        json.inf.taskName[hitApp] = { 'tasksQtd': tasksQtdHitApp, 'tasksSec': tasksSecHitApp }
                         infConfigStorage = { 'path': `./log/TryRating/${time1}/###_JSON_###.json`, 'functionLocal': false, 'action': 'set', 'key': 'tryRating', 'value': json }
-                        retConfigStorage = await configStorage(infConfigStorage); let tasksSec = 0, tasksQtd = 0
-                        json[nameTask].tasks.map(async (value, index) => { tasksQtd = tasksQtd + 1; tasksSec = tasksSec + value.sec })
+                        retConfigStorage = await configStorage(infConfigStorage);
+
+                        json.inf.reg['tasksHour'] = `${secToHour(tasksSec).res}`
+                        infConfigStorage = { 'path': `./log/TryRating/MES_${time.mon}_${time.monNam}/###_JSON_###.json`, 'functionLocal': false, 'action': 'set', 'key': `DIA_${time.day}`, 'value': json.inf.reg }
+                        retConfigStorage = await configStorage(infConfigStorage);
+
+                        infConfigStorage = { 'path': `./log/TryRating/MES_${time.mon}_${time.monNam}/###_JSON_###.json`, 'functionLocal': false, 'action': 'get', 'key': `*` }
+                        retConfigStorage = await configStorage(infConfigStorage);
+                        for (const nameKey in retConfigStorage.res) { tasksQtdMon += retConfigStorage.res[nameKey].tasksQtd; tasksSecMon += retConfigStorage.res[nameKey].tasksSec }
+
                         const sendWeb = {
                             "fun": {
                                 "securityPass": securityPass,
@@ -158,10 +185,9 @@ try {
                                 "funRun": {
                                     "name": "notification",
                                     "par": {
-                                        "duration": 3,
-                                        "title": `SPAM | ${nameTask}`,
-                                        "message": `QTD: ${tasksQtd} | TIME: ${secToHour(tasksSec).res} | ~SEC ${(tasksSec / tasksQtd).toFixed(0)}`,
-                                        "iconUrl": "./src/media/notification_3.png",
+                                        "duration": 3, "iconUrl": "./src/media/icon_4.png",
+                                        "title": `SPAM | ${hitApp}`,
+                                        "message": `QTD: ${tasksQtdMon.toString().padStart(4, '0')} | TOTAL: ${secToHour(tasksSecMon).res}\nQTD: ${tasksQtd.toString().padStart(4, '0')} | TOTAL: ${secToHour(tasksSec).res} | MÉDIO: ${secToHour((tasksSecHitAppLast / tasksQtdHitAppLast).toFixed(0)).res}`
                                     }
                                 }
                             }
@@ -186,11 +212,7 @@ try {
                     // ######################################################################
                     if (!ret.send) { console.log('REQUISIAO CANCELADA') } else if ((ret.res) && (ret.res.body || ret.res.headers)) { console.log('REQUISIAO ALTERADA') }
                 } else { console.log('OUTRO URL |', inf.url) }
-            } catch (e) {
-                console.log(regexE({ 'e': e }).res)
-            }
-
-            return ret
+            } catch (e) { console.log(regexE({ 'e': e }).res) }; return ret
         }
 
         // -------------------------------------------------------------------------------------------------
@@ -328,7 +350,8 @@ try {
 
     }
     await run()
-} catch (e) {
+}
+catch (e) {
     console.log(regexE({ 'e': e }).res)
 }
 

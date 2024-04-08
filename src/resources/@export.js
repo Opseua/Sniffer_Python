aa0fe08e0f4b0b6fcbcc8a41304f8153baacc53b
Orlando <opseua@gmail.com>
2024-04-07 23:37:20 -0300

    Novo modelo de export

--- src/resources/@export.js
+++ src/resources/@export.js
@@ -1,11 +1,16 @@
 // FUNCTIONS
-await import('../../../Chrome_ExtensionOld/src/resources/@functions.js');
+await import('../../../Chrome_Extension/src/resources/@functions.js');
 
 // DEFINIR → LETTER | ROOT | FUNCTION | PROJECT | FILE | LINE
 // let retGetPathNew = await getPathNew({ 'e': new Error(), 'isFunction': false, })
 // globalWindow.devResWs = cng == 1 ? 'CHROME' : 'SNIFFER';
 
+let infChat, retChat
+infChat = { 'provider': 'open.ai', 'input': `Qual a idade de Marte?` };
+retChat = await chat(infChat);
+console.log(retChat)
+
 // FUNÇÕES DESSE PROJETO
-await import('../scripts/@ewoq.js')
-await import('../scripts/@tryRating.js')
-await import('../scripts/tryRating_Search20.js');
+// await import('../scripts/@ewoq.js')
+// await import('../scripts/@tryRating.js')
+// await import('../scripts/tryRating_Search20.js');
